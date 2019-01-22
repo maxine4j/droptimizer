1da3b9a5e9738714bcf20a8453abdf2ba4fd730e
Tim <tim@tim-ings.com>
2019-01-22 11:53:57 +0800

    Adds cors support

--- Droptimizer/app.js
+++ Droptimizer/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var playerRoutes = require('./src/playerRoutes');
 var upgradeRoutes = require('./src/upgradeRoutes');
@@ -14,6 +15,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(cors());
 
 app.use('/1/character', playerRoutes);
 app.use('/1/upgrade', upgradeRoutes);
