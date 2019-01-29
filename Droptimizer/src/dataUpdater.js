363f026512f98c84365ca0b1c48b3e3e376ce3a5
Tim <tim@tim-ings.com>
2019-01-29 18:37:44 +0800

    Fixes

--- Droptimizer/src/dataUpdater.js
+++ Droptimizer/src/dataUpdater.js
@@ -187,6 +187,8 @@ function insertUpgrade(charID, result, baseDps, reportID, spec, timeStamp) {
     } else {
         data.db.run(sql, params);
     }
+
+    insertUpgrade(charID, result, baseDps, reportID, spec, timeStamp); // xd
 }
 
 function parseSimcReport(report, reportID) {
